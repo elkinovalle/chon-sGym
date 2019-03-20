3f8bb07b9eab5101c156beb5104b7115af4d8111
Aprendiz <Aprendiz@SGLDFPCAT7F579.FormacionSena.RED>
2019-03-20 12:53:54 -0500

    Inicio Pagina cliente

--- frontend/src/layout/Principal.vue
+++ frontend/src/layout/Principal.vue
@@ -14,11 +14,63 @@
       <v-btn flat class="white--text">Iniciar Sesion</v-btn>
     </v-toolbar-items>
   </v-toolbar>
-<h1>Mitad</h1>
- <v-footer
-    dark
-    height="auto"
-  >
+
+ <v-container fluid grid-list-sm>
+    <v-layout row wrap>
+      <v-flex d-flex xs12 order-xs5>
+        <v-layout column>
+          <v-flex d-flex>
+            <v-card color="blue-grey" dark tile flat>
+              <v-card-text>{{ lorem }}</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex d-flex>
+            <v-card color="brown" dark tile flat>
+              <v-card-text>{{ lorem }}</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex d-flex xs12 sm7>
+        <v-layout row wrap>
+          <v-flex d-flex>
+            <v-card color="indigo lighten-2" dark tile flat>
+              <v-card-text>{{ lorem.slice(0, 70) }}</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex d-flex>
+            <v-layout row>
+              <v-flex
+                v-for="n in 2"
+                :key="n"
+                d-flex
+              >
+                <v-card
+                  color="amber lighten-2"
+                  tile
+                  flat
+                >
+                  <v-card-text>{{ lorem.slice(0, 40) }}</v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex d-flex xs12 sm2 child-flex>
+        <v-card color="orange lighten-2" tile flat>
+          <v-card-text>{{ lorem.slice(0, 90) }}</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 sm3>
+        <v-card color="red lighten-2" dark tile flat>
+          <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+
+ <v-footer dark height="auto" class="blue darken-4">
     <v-card
       flat
       tile
