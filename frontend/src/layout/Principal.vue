8a16bf01d3785a892c360775efdcadacd3e89f94
Aprendiz <Aprendiz@SGLDFPCAT7F579.FormacionSena.RED>
2019-03-20 14:44:38 -0500

    Adelanto footer cliente

--- frontend/src/layout/Principal.vue
+++ frontend/src/layout/Principal.vue
@@ -15,47 +15,13 @@
     </v-toolbar-items>
   </v-toolbar>
 
-<v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">12</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 2" :key="`6${i}`" xs6>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">6</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 3" :key="`4${i}`" xs4>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">4</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 4" :key="`3${i}`" xs3>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">3</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 6" :key="`2${i}`" xs2>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">2</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 12" :key="`1${i}`" xs1>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">1</v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
-
- <v-footer dark height="auto" class="blue darken-4">
+ <v-footer dark height="auto" >
     <v-card
       flat
       tile
-      class="indigo lighten-1 white--text text-xs-center"
+      class="class= blue darken-4 white--text text-xs-center"
     >
+    <h1 class="white--text body-1">SIGUENOS EN NUESTRAS REDES:</h1>
       <v-card-text>
         <v-btn
           v-for="icon in icons"
@@ -65,16 +31,27 @@
         >
           <v-icon size="24px">{{ icon }}</v-icon>
         </v-btn>
-      </v-card-text>
 
+      </v-card-text>
+       <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        flat
+        round
+      >
+        {{ link }}
+      </v-btn>
       <v-card-text class="white--text pt-0">
+        <strong class="subheading">Get connected with us on social networks!</strong><br>
+        <strong class="subheading">Get connected with us on social networks!</strong>
         Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
       </v-card-text>
 
       <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        &copy;2018 — <strong>Vuetify</strong>
+      <v-card-text class="white--text caption">
+        &copy;COPYRIGHT CHON'S GYM © 2019 · Desarrollo de página web y posicionamiento SEO por ADSI-GROUP · TODOS LOS DERECHOS RESERVADOS · POLÍTICA DE PRIVACIDAD.
       </v-card-text>
     </v-card>
   </v-footer>
@@ -82,15 +59,31 @@
 </template>
 <script>
 export default {
-  
+  data: () => ({
+    icons: [
+      'fab fa-facebook',
+      'fab fa-twitter',
+      'fab fa-google-plus',
+      'fab fa-youtube',
+      'fab fa-instagram'
+    ],
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us'
+    ]
+  })
+
 }
 </script>
 <style lang="stylus">
  .v-btn{
     font-size 16px !important
-    font-family Helvetica 
+    font-family Helvetica
     font-style italic
   }
 
 </style>
-
