ea8daa938dbd7f7bd3bc625588290e28005edf2f
Elkin <edovalle08@misena.edu.co>
2019-04-02 12:50:56 -0500

    avnaces

---
+++ frontend/src/views/ValoracionEmpleado.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+          <v-btn color="green darken-4" class=" white--text display-1" >Registrar rutina</v-btn>
+          <v-btn color="red darken-4" class=" white--text display-1" >Cancelar</v-btn>
+
+    </div>
+</template>
+<script>
+export default {
+  created () {
+    this.$store.commit('SET_LAYOUT', 'empleado-layout')
+  },
+  data () {
+    return {
+      select: 'Masaje',
+      items: [
+        'Masaje',
+        'Hidroterapia',
+        'Plan 1 mes',
+        'Plan 3 meses'
+      ]
+    }
+  }
+
+}
+</script>
+<style lang="stylus">
+
+</style>
