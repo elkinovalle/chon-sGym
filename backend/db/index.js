28eb6eda356f2e281580186dec4340a32e59e921
elkinovalle <edovalle08@misena.edu.co>
2019-04-30 14:14:43 -0500

    Llamar tablas backend

--- backend/db/index.js
+++ backend/db/index.js
@@ -14,6 +14,18 @@ module.exports = async function (config) {
   const ReservationModel = setupReservationModel(config)
   const ProductModel = setupProductModel(config)
   const Detail_SaleModel = setupDetail_SaleModel(config)
+  const CityModel = setupCityModel(config)
+  const ClassModel = setupClass(config)
+  const CountryModel = setupCountryModel(config)
+  const DepartmentModel = setupDepartmentModel(config)
+  const Detail_MembershipModel = setupDetail_MembershipModel(config)
+  const MembershipModel = setupMembershipModel(config)
+  const SaleModel = setupSaleModel(config)
+  const ScheduleModel = setupScheduleModel(config)
+  const Type_ClassModel = setupType_ClassModel(config)
+  const Type_MembershipModel = setupType_MembershipModel(config)
+  const DayModel = setupDayModel(config)
+
 
   //relaciones de usuario a revervas
   UserModel.hasMany(ReservationModel,{ foreignKey: 'usuarios' })
