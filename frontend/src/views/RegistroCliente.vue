1a75c1f07727a6ad0b75554fcb441b5b340c4e48
Aprendiz <Aprendiz@SGLDFPCAT7F579.FormacionSena.RED>
2019-05-14 17:09:09 -0500

    Registro cliente completado

--- frontend/src/views/RegistroCliente.vue
+++ frontend/src/views/RegistroCliente.vue
@@ -86,13 +86,28 @@
       <v-card-actions class="botones">
         <v-btn class="font-weight-black title font-italic" flat @click="resetForm">Cancelar</v-btn>
         <v-spacer></v-spacer>
-        <v-btn
+        <v-dialog v-model="dialogEmail" persistent max-width="600px">
+          <template v-slot:activator="{ on }">
+         <v-btn
+          v-on="on"
           flat
           color="blue darken-4"
           type="submit"
           class="font-weight-black title font-italic"
           :disabled="$v.$invalid"
         >Registrarse</v-btn>
+        </template>
+          <v-card class="contenido">
+            <v-card-title>
+              <span class="headline">¿Desea completar el registro?</span>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-4" flat @click="dialogEmail = false" class="title" to="/planes-cliente">Omitir</v-btn>
+              <v-btn color="blue darken-4" flat @click="dialogEmail = false" class="title" to="/editar-perfil-cliente"> Si</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-card-actions>
             </v-card>
           </v-flex>
@@ -161,6 +176,7 @@ export default {
       isLoading: false,
       allowSpaces: false,
       dialog: false,
+      dialogEmail: false,
       show1: false,
       show2: false,
       gender: ['M', 'F'],
@@ -256,5 +272,19 @@ export default {
 }
 .botones{
   padding 0px 30px 30px 30px
+}
+.contenido{
+  padding 30px
+}
+.ancho{
+  width 120px!important
+}
+.imagen{
+  width 130px !important
+  height 150px !important
+  margin -20px 0px 0px -40px
+}
+.margen{
+  margin 80px 0px
 }
 </style>
