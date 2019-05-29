341594de8b9cb6d5188c17d548d7e7dda2702521
sebastian arias <jsarias98@misena.edu.co>
2019-05-29 14:13:32 -0500

    datos en la tabla

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    layout: 'principal-layout'
+    layout: 'principal-layout',
+    plans: []
   },
   mutations: {
     SET_LAYOUT (state, newLayout) {
       state.layout = newLayout
+    },
+    SET_PLANS (state, newPlan) {
+      state.plans = newPlan
     }
   },
   actions: {
 
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
